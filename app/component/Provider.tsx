16eeee3fc9d271472b2fdf000bbfc87fc13e2793
Koushik Sarkar <koushikjit@gmail.com>
2023-10-29 08:49:45 +0530

    firs commit

---
+++ app/component/Provider.tsx
@@ -0,0 +1,14 @@
+"use client"
+import { SessionProvider } from "next-auth/react"
+
+type Props = {
+    children: React.ReactNode
+}
+
+const Provider = (props: Props) => {
+  return (
+    <SessionProvider>{props.children}</SessionProvider>
+  )
+}
+
+export default Provider
